b57db545fae9207e95de5367b1cc5db18a6c5782
alexfed33 <38166639+alexfed33@users.noreply.github.com>
2022-02-08 03:02:54 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -11,7 +11,6 @@ function get_user_by_email($email) {
     return $user;
 }
 
-
 function set_flash_message($name, $message) {
     $_SESSION[$name] = $message;
 }
@@ -36,5 +35,50 @@ function display_flash_message($name) {
         echo "<div class=\"alert alert-{$name} text-dark\" role=\"alert\">{$_SESSION[$name]} </div>";
         unset($_SESSION[$name]);
     }
-
 }
+
+function login($email, $password) {
+    $pdo = new PDO('mysql:host=localhost;dbname=project', 'root', '');
+
+    $result = $pdo->query('SELECT * FROM users');
+
+    foreach ($result as $users) {
+        if ($email == $users['email'] and password_verify($password, $users['password'])) {
+
+            $_SESSION['auth'] = true;
+            $_SESSION['role'] = $users['role'];
+            $_SESSION['id'] = $users['id'];
+            $_SESSION['user'] = $users['email'];
+
+            redirect_to("users.php");
+            exit;
+            }
+        };
+
+        if (empty($user)) {
+            set_flash_message("danger", "Такого пользователя не существует");
+            redirect_to("page_login.php");
+            exit;
+        }
+}
+
+function check_auth() {
+    if($_SESSION['auth'] == false) {
+        redirect_to("page_login.php");
+    }
+}
+
+function query_users() {
+    $pdo = new PDO('mysql:host=localhost;dbname=project', 'root', '');
+    $result = $pdo->query('SELECT * FROM users');
+
+    return $result;
+}
+
+function logout() {
+    unset($_SESSION['auth']);
+    session_destroy();
+    redirect_to("page_login.php");
+}
+
+
