22a0f762e22c1c85f55aac8156919334e7d462eb
alexfed33 <38166639+alexfed33@users.noreply.github.com>
2022-02-13 04:14:02 +0300

    Add files via upload

---
+++ security_edit.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+require "functions.php";
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+$user_id = $_SESSION['user_id'];
+
+
+if ($_SESSION['email'] == $email) {
+    edit_credentials($email, $password, $user_id);
+} else {
+        $user = get_user_by_email($email);
+        if (!empty($user)) {
+            set_flash_message('danger', 'Такой email уже занят');
+            redirect_to("security.php?id=$user_id");
+            exit();
+        }
+        edit_credentials($email, $password, $user_id);
+}
+
+
+
+
+
+
+
+
+
+
+
